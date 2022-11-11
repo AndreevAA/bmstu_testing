85ea11b2ed81f047040dfa770b048db0496dc55e
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2022-11-11 21:18:05 +0300

    Updated Buildes for testing with pytest and working with database

---
+++ Test/Builders/LookBuilder.py
@@ -0,0 +1,13 @@
+class LookBuilder:
+    clothes_id = []
+    look_name = ""
+    image = None
+
+    def with_name(self, name):
+        self.look_name = name
+        return self
+
+    def with_id(self, ids):
+        self.clothes_id = ids
+        return self
+        
